7eed012e71ec2507eace719c336914971855d5ff
rath772k <bharathprithivi@gmail.com>
2024-06-27 03:09:07 +0530

    add simple transformer code

--- transformer.py
+++ transformer.py
@@ -1,41 +1,98 @@
-from collections import defaultdict
-import numpy as np
-import time
+import math
 import torch
+import torch.nn.functional as f
+from torch import nn
+import torch.utils
 
+class CausalSelfAttention(nn.Module):
+    """
+    Multiply your embeddings[i] with Q, K and V to get q[i], k[i] and v[i] respectively
+    score[i][j] = q[i] . k[j] , when  j <= i else 0
+    norm_score[i] = softmax(score[i])
+    final_value[i] = sum(norm_score[i][j] * v[j])
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        st = time.perf_counter()
-        res = func(*args, **kwargs)
-        en = time.perf_counter()
-        return res, en - st
-    return wrapper
+    Let us ignore the j <= i condition for now.
+    Then our the above calculation is simply
+    final_value = emdeddings . (softmax(q . k_T) . v )
 
-@timer
-def matmul(t1, t2):
-    return t1.matmul(t2)
+    To enforce the j <=i condition, we can just set the j > i part to be -inf before applying softmax
 
-if __name__ == "__main__":
-    t1 = torch.rand([10000, 500])
-    t2 = torch.rand([500, 20000])
-    n_samples = 100
+    There is an additional scaling factor attn_size to be divided.
+    This ensures unit standar deviation and prevents the softmax function to be spiky
 
-    devices = ["cpu", "cuda"]
-    times = defaultdict(list)
-    for device in devices:
-        for _ in range(n_samples):
-            result, time_taken = matmul(t1.to(device=device), t2.to(device=device))
-            times[device].append(time_taken)
-        print(f"{device} percentiles: {np.percentile(times[device], q=[0.50, 0.90, 0.95, 0.99])}")
+    **NOTE:** In the original Attention is All You Need paper, values is of a different dimension which is then
+    projected using a linear layer, but here we'll make sure that attn_size is a factor of embed_size
+    """
+    def __init__(self, embed_size, attn_size, context_length):
+        super().__init__()
+        assert embed_size % attn_size == 0
+        self.Q = nn.Linear(embed_size, attn_size, bias=False)
+        self.K = nn.Linear(embed_size, attn_size, bias=False)
+        self.V = nn.Linear(embed_size, attn_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(context_length, context_length)).view(1, context_length, context_length))
     
-    speedups = [cpu / cuda for cpu, cuda in zip(times["cpu"], times["cuda"])]
-    print(f"speedup percentiles: {np.percentile(speedups, q=[0.50, 0.90, 0.95, 0.99])}")
+    def forward(self, embeddings):
+        # embeddings -> (batch_size, context_length, embed_size)
+        q = self.Q(embeddings) # (batch_size, context_length, attn_size)
+        k = self.K(embeddings) # (batch_size, context_length, attn_size)
+        v = self.V(embeddings) # (batch_size, context_length, attn_size)
+        _, context_length, attn_size = q.shape
+        scores = q @ k.transpose(-2, -1) * (1 / math.sqrt(float(attn_size))) # (batch_size, context_length, context_length)
+        masked_scores = torch.where(self.tril[:, :context_length, :context_length] == 0, float("-inf"), scores) # (batch_size, context_length, context_length)
+        norm_scores = f.softmax(masked_scores, dim=-1) # (batch_size, context_length, context_length)
+        final_values = norm_scores @ v # (batch_size, context_length, attn_size)
+        return final_values
+        
+class AttentionBlock(nn.Module):
+    def __init__(self, embed_size, num_heads, context_length):
+        super().__init__()
+        attn_size = embed_size // num_heads
+        self.attn_heads = nn.ModuleList((CausalSelfAttention(embed_size, attn_size, context_length)) for _ in range(num_heads))
+        self.fcn = nn.Linear(embed_size, embed_size)
+        self.layer_norm_1 = nn.LayerNorm(embed_size, bias=False)
+        self.layer_norm_2 = nn.LayerNorm(embed_size, bias=False)
+        self.num_heads = num_heads
 
-"""
-Output for RTX 4050 6GiB (80 W):
---------------------------------
-cpu percentiles: [0.30203317 0.30234825 0.30238763 0.30241914]
-cuda percentiles: [7.73454349e-05 7.74297829e-05 7.74403264e-05 7.74487612e-05]
-speedup percentiles: [47.07481633 74.47030797 77.89474442 80.63429359]
-"""
+    def forward(self, embeddings):
+        x = embeddings + torch.cat([self.attn_heads[i](embeddings) for i in range(self.num_heads)], dim=-1)
+        x = x + self.fcn(self.layer_norm_1(x))
+        x = self.layer_norm_2(x)
+        return x
+    
+
+class Transformer(nn.Module):
+    def __init__(self, vocab_size, embed_size, num_heads, context_length, num_layers):
+        super().__init__()
+        self.token_embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
+        self.pos_embedding = nn.Embedding(num_embeddings=context_length, embedding_dim=embed_size)
+        self.attn_blocks = nn.ModuleList((AttentionBlock(embed_size, num_heads, context_length) for _ in range(num_layers)))
+        self.lm_head = nn.Linear(embed_size, vocab_size, bias=False)
+        self.context_length = context_length
+        self.num_layers = num_layers
+    
+    def forward(self, inputs, targets=None):
+        # inputs.shape -> (batch_size, context_length)
+        _, context_length = inputs.shape
+        x = self.token_embedding(inputs) + self.pos_embedding(torch.arange(context_length, device=inputs.device))
+        for attn_block in self.attn_blocks:
+            x = attn_block(x)
+        logits =  self.lm_head(x)
+        # logits.shape -> (batch_size, context_length, vocab_size)
+        # but for torch's cross entropy loss, need (batch_size, vocab_size, context_length)
+        if targets is None:
+            return logits, None
+        logits = logits.permute(0, 2, 1)
+        loss = f.cross_entropy(logits, targets)
+        logits = logits.permute(0, 2, 1)
+        return logits, loss
+    
+    def generate(self, start_idx, max_tokens=100):
+        for _ in range(max_tokens):
+            logits, _ = self(start_idx[:, -self.context_length:]) # we only care till context length
+
+            # we only care about last prediction
+            pred = logits[:, -1, :] # (batch_size, vocab_size)
+            prob = f.softmax(pred, dim=-1)
+            idx = prob.multinomial(num_samples=1) # (batch_size, 1)
+            start_idx = torch.cat([start_idx, idx], dim=-1)
+        return start_idx
