7a4c4b40bf2001ca3c8a9a6aa433c76172d97dd6
rath772k <bharathprithivi@gmail.com>
2024-06-29 17:16:12 +0530

    separate out tokenizer from notebook

---
+++ language_models/tokenizers/base_tokenizer.py
@@ -0,0 +1,7 @@
+class BaseTokenizer:
+    def encode(self, *args, **kwargs):
+        raise NotImplementedError
+    
+    def decode(self, *args, **kwargs):
+        raise NotImplementedError
+    
---
+++ language_models/tokenizers/character_tokenizer.py
@@ -0,0 +1,23 @@
+from tokenizers.base_tokenizer import BaseTokenizer
+
+class CharacterTokenizer(BaseTokenizer):
+    def __init__(self, char_to_token, token_to_char, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.char_to_token = char_to_token
+        self.token_to_char = token_to_char
+        self.vocab_size = len(self.char_to_token)
+
+    def encode(self, text):
+        return [self.char_to_token[char] for char in text]
+
+    def decode(self, tokens):
+        return "".join(self.token_to_char[token] for token in tokens)
+    
+    @classmethod
+    def build_tokenizer(cls, filepath):
+        with open(filepath, "r") as f:
+            data = f.read()
+        vocab = set(data)
+        char_to_token = {char: idx for idx, char in enumerate(vocab)}
+        token_to_char = {idx: char for char, idx in char_to_token.items()}
+        return cls(char_to_token, token_to_char)
